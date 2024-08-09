db3a389e77843a82202133dfcf9ad055eeb4257d
murny <1930474+murny@users.noreply.github.com>
2024-08-09 15:57:41 -0600

    fixes for asset building

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,10 +15,10 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries'),
-  ]
+  // plugins: [
+  //   require('@tailwindcss/forms'),
+  //   require('@tailwindcss/aspect-ratio'),
+  //   require('@tailwindcss/typography'),
+  //   require('@tailwindcss/container-queries'),
+  // ]
 }
