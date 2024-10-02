71d56e63b50bc5dc6386ab3edbcbacd791f50245
murny <1930474+murny@users.noreply.github.com>
2024-10-02 13:12:31 -0600

    Simplify authentication code

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -7,7 +7,7 @@ module Admin
     private
 
     def require_admin
-      redirect_to root_path, alert: t("admin.base.unauthorized") unless current_user.can_administer?
+      redirect_to root_path, alert: t("admin.base.unauthorized") unless Current.user.can_administer?
     end
   end
 end
