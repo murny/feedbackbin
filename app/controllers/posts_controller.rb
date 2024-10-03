d2d4853e6924cd0c62afc8bb07f2047afb2491cc
murny <1930474+murny@users.noreply.github.com>
2024-10-03 13:01:28 -0600

    Use new params expects syntax in a few places

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -64,11 +64,11 @@ class PostsController < ApplicationController
 
   # Use callbacks to share common setup or constraints between actions.
   def set_post
-    @post = Post.find(params[:id])
+    @post = Post.find(params.expect(:id))
   end
 
   # Only allow a list of trusted parameters through.
   def post_params
-    params.require(:post).permit(:title, :description)
+    params.expect(post: [:title, :description])
   end
 end
