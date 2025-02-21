63f9393f13379cd95d19a95cbf21cd6e1aa7c398
murny <1930474+murny@users.noreply.github.com>
2025-02-20 22:57:51 -0700

    Remove button size option, refactor docs

--- app/helpers/components/button_helper.rb
+++ app/helpers/components/button_helper.rb
@@ -1,6 +1,6 @@
 module Components
   module ButtonHelper
-    def render_button(label = "", text: nil, variant: :primary, as: :button, href: nil, size: :medium, data: {}, **options, &block)
+    def render_button(label = "", text: nil, variant: :primary, as: :button, href: nil, data: {}, **options, &block)
       button_classes = "btn"
 
       variant_classes = case variant.to_sym
@@ -16,16 +16,7 @@ module Components
         "btn--plain"
       end
 
-      size_classes = case size.to_sym
-      when :small
-        "btn--sm"
-      when :medium
-        "" # Default size, no additional class needed
-      when :large
-        "btn--lg"
-      end
-
-      button_classes = [ button_classes, variant_classes, size_classes, options[:class] ].reject(&:blank?).join(" ")
+      button_classes = [ button_classes, variant_classes, options[:class] ].reject(&:blank?).join(" ")
 
       text = label if label.present?
       text = capture(&block) if block
