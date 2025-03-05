0b84c9c19b9271e68401907e27afce94934d1f5e
murny <1930474+murny@users.noreply.github.com>
2025-03-04 17:23:35 -0700

    Enhance admin documentation and styling
    - Add new Tailwind CSS theme and component styles
    - Implement admin dashboard with detailed layout
    - Create documentation pages for components (accordion, breadcrumb, button, toast)
    - Update admin layout and sidebar navigation
    - Refactor admin routes and controllers

--- app/controllers/admin/docs_controller.rb
+++ app/controllers/admin/docs_controller.rb
@@ -4,5 +4,11 @@ module Admin
   class DocsController < Admin::BaseController
     def index
     end
+
+    def show
+      @document = params[:id]
+
+      render "admin/docs/#{@document}"
+    end
   end
 end
--- test/controllers/admin/docs/components_controller_test.rb
+++ test/controllers/admin/docs/components_controller_test.rb
@@ -4,7 +4,7 @@ require "test_helper"
 
 class Admin::Docs::ComponentsControllerTest < ActionDispatch::IntegrationTest
   test "should not get show when not logged in" do
-    get admin_docs_component_path(id: "buttons")
+    get admin_docs_component_path("button")
 
     assert_response :not_found
   end
@@ -12,7 +12,7 @@ class Admin::Docs::ComponentsControllerTest < ActionDispatch::IntegrationTest
   test "should not get show as non admin user" do
     sign_in_as users(:one)
 
-    get admin_docs_component_path(id: "buttons")
+    get admin_docs_component_path("button")
 
     assert_response :not_found
   end
@@ -20,7 +20,7 @@ class Admin::Docs::ComponentsControllerTest < ActionDispatch::IntegrationTest
   test "should get show as admin for buttons component" do
     sign_in_as users(:shane)
 
-    get admin_docs_component_path(id: "buttons")
+    get admin_docs_component_path("button")
 
     assert_response :success
   end
@@ -28,7 +28,7 @@ class Admin::Docs::ComponentsControllerTest < ActionDispatch::IntegrationTest
   test "should get show as admin for alert component" do
     sign_in_as users(:shane)
 
-    get admin_docs_component_path(id: "alert")
+    get admin_docs_component_path("toast")
 
     assert_response :success
   end
@@ -37,7 +37,7 @@ class Admin::Docs::ComponentsControllerTest < ActionDispatch::IntegrationTest
     sign_in_as users(:shane)
 
     assert_raises(ActionView::MissingTemplate) do
-      get admin_docs_component_path(id: "non_existent_component")
+      get admin_docs_component_path("non_existent_component")
     end
   end
 end
