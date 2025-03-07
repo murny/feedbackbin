1bc05551c317c2957cc58997f36e627b55c5fa32
murny <1930474+murny@users.noreply.github.com>
2025-03-07 15:59:17 -0700

    Refactor admin documentation routes and controllers
    - Remove Admin::Docs::ComponentsController
    - Update Admin::DocsController to handle individual documentation pages
    - Modify routes to use singular resource with custom actions
    - Add new documentation view templates for introduction, installation, configuration, and deploying
    - Update sidebar navigation links to use new routes
    - Add corresponding controller tests for new documentation pages

--- app/controllers/admin/docs_controller.rb
+++ app/controllers/admin/docs_controller.rb
@@ -2,13 +2,5 @@
 
 module Admin
   class DocsController < Admin::BaseController
-    def index
-    end
-
-    def show
-      @document = params[:id]
-
-      render "admin/docs/#{@document}"
-    end
   end
 end
