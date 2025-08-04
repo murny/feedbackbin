64744bb368ebb4bca9d15d46ad57e703be4fa374
murny <1930474+murny@users.noreply.github.com>
2025-08-04 16:17:56 -0600

    Seed testing and minor fixes
    Collapse migrations again

--- db/migrate/20250729180702_add_pinned_to_posts.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class AddPinnedToPosts < ActiveRecord::Migration[8.1]
-  def change
-    add_column :posts, :pinned, :boolean, default: false, null: false
-    add_index :posts, :pinned
-  end
-end
