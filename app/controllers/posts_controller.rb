ed812fbbe10a64a9b8cc7b01fe350d2566d07beb
murny <1930474+murny@users.noreply.github.com>
2025-08-06 12:48:22 -0600

    Update sorting logic in posts controller and enhance time display in views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,15 +11,15 @@ class PostsController < ApplicationController
     authorize Post
 
     posts = Current.organization.posts
-    @categories = Current.organization.categories
-    @post_statuses = Current.organization.post_statuses
+    @categories = Current.organization.categories.order(:name)
+    @post_statuses = Current.organization.post_statuses.order(:position)
 
     if params[:category_id].present?
       @category = @categories.find_by(id: params[:category_id])
       posts = posts.where(category_id: @category.id) if @category
     end
 
-    posts = posts.sort_by_params(params[:sort], sort_direction)
+    posts = posts.sort_by_params(sort_column(Post), sort_direction)
 
     # Apply status filtering if provided
     if params[:post_status_id].present?
--- app/helpers/time_helper.rb
+++ app/helpers/time_helper.rb
@@ -1,7 +1,34 @@
 # frozen_string_literal: true
 
 module TimeHelper
+  include ActionView::Helpers::DateHelper
+
   def local_datetime_tag(datetime, style: :time, **attributes)
     tag.time(**attributes, datetime: datetime.iso8601, data: { local_time_target: style })
+  end
+
+  # Creates a time ago display with a tooltip showing the full timestamp
+  #
+  # @param datetime [Time, DateTime] The time to display
+  # @param **attributes [Hash] Additional HTML attributes for the time tag
+  # @return [ActiveSupport::SafeBuffer] HTML time tag with time ago text and full datetime tooltip
+  #
+  # Example:
+  #   time_ago_with_tooltip(post.created_at)  # => "3 days ago" with tooltip showing full date
+  def time_ago_with_tooltip(datetime, **attributes)
+    relative_text = relative_time_in_words(datetime)
+    full_datetime = datetime.strftime("%B %d, %Y at %l:%M %p").strip
+
+    # Merge any custom classes with our base classes
+    css_classes = "cursor-help"
+    css_classes += " #{attributes[:class]}" if attributes[:class].present?
+
+    tag.time(
+      relative_text,
+      title: full_datetime,
+      datetime: datetime.iso8601,
+      class: css_classes,
+      **attributes.except(:class)
+    )
   end
 end
