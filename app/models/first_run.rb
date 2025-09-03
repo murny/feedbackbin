80c1db1fa5902dcb67b220a923fc24f845fc83dc
murny <1930474+murny@users.noreply.github.com>
2025-09-03 15:33:12 -0600

    Wrap Firstrun creation in transaction so we either all succeed or rollback

--- app/models/first_run.rb
+++ app/models/first_run.rb
@@ -5,11 +5,13 @@ class FirstRun
   FIRST_CATEGORY_NAME = "Feature Requests"
 
   def self.create!(user_params)
-    user = User.create!(user_params)
-    Organization.create!(
-      name: ORGANIZATION_DEFAULT_NAME,
-      owner: user,
-      categories_attributes: [ { name: FIRST_CATEGORY_NAME } ]
-    )
+    ApplicationRecord.transaction do
+      user = User.create!(user_params)
+      Organization.create!(
+        name: ORGANIZATION_DEFAULT_NAME,
+        owner: user,
+        categories_attributes: [ { name: FIRST_CATEGORY_NAME } ]
+      )
+    end
   end
 end
