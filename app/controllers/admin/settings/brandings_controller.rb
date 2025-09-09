d1fb940760239cc442f1cea67ecd4631f19e7fd3
murny <1930474+murny@users.noreply.github.com>
2025-09-09 17:24:54 -0600

    Add branding settings page for organization admin area

---
+++ app/controllers/admin/settings/brandings_controller.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module Admin
+  module Settings
+    class BrandingsController < Admin::BaseController
+      def show
+      end
+
+      def update
+        if Current.organization.update(organization_params)
+          redirect_to admin_settings_branding_path, notice: t(".successfully_updated")
+        else
+          render :show, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+        def organization_params
+          params.require(:organization).permit(:name, :logo, :subdomain, :domain).compact
+        end
+    end
+  end
+end
---
+++ test/controllers/admin/settings/brandings_controller_test.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+module Admin
+  module Settings
+    class BrandingsControllerTest < ActionDispatch::IntegrationTest
+      setup do
+        sign_in_as users(:shane)
+      end
+
+      test "should get show" do
+        get admin_settings_branding_url
+
+        assert_response :success
+      end
+
+      test "should update branding" do
+        patch admin_settings_branding_url, params: {
+          organization: {
+            name: "New Name",
+            subdomain: "newsub",
+            domain: "example.com"
+          }
+        }
+
+        assert_redirected_to admin_settings_branding_url
+        follow_redirect!
+
+        assert_response :success
+        assert_equal "Branding settings were successfully updated.", flash[:notice]
+      end
+
+      test "should upload logo" do
+        file = fixture_file_upload("random.jpeg", "image/jpeg")
+
+        patch admin_settings_branding_url, params: { organization: { logo: file } }
+
+        assert_redirected_to admin_settings_branding_url
+        assert_equal "Branding settings were successfully updated.", flash[:notice]
+      end
+
+      test "should not allow non-admin" do
+        sign_in_as users(:two)
+
+        patch admin_settings_branding_url, params: { organization: { name: "New Name" } }
+
+        assert_redirected_to root_url
+        assert_equal "You are not authorized to perform this action.", flash[:alert]
+      end
+    end
+  end
+end
