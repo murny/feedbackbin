f6d03a8ca1e9e16990ca1e670375926dcf051072
murny <1930474+murny@users.noreply.github.com>
2025-09-11 13:43:06 -0600

    Update sidebar navigation to include links for organization invitations and other management sections.

--- app/controllers/admin/settings/organization_invitations_controller.rb
+++ app/controllers/admin/settings/organization_invitations_controller.rb
@@ -3,22 +3,29 @@
 module Admin
   module Settings
     class OrganizationInvitationsController < Admin::BaseController
-      def new
-        authorize OrganizationInvitation
+      def index
+        @organization_invitations = Current.organization.organization_invitations.includes(:invited_by).order(created_at: :desc)
+      end
 
+      def new
         @organization_invitation = OrganizationInvitation.new(organization: Current.organization)
       end
 
       def create
-        authorize OrganizationInvitation
-
         @organization_invitation = OrganizationInvitation.new(organization: Current.organization, invited_by: Current.user, **organization_invitation_params)
 
         if @organization_invitation.save
-          redirect_to admin_settings_memberships_path, notice: t(".successfully_created")
+          redirect_to admin_settings_organization_invitations_path, notice: t(".successfully_created")
         else
           render :new, status: :unprocessable_entity
         end
+      end
+
+      def destroy
+        @organization_invitation = Current.organization.organization_invitations.find(params[:id])
+
+        @organization_invitation.destroy
+        redirect_to admin_settings_organization_invitations_path, notice: t(".successfully_destroyed")
       end
 
       private
