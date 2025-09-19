b7016acf8c6cc6eb4897399a35f6779a41b8710f
murny <1930474+murny@users.noreply.github.com>
2025-09-18 18:17:06 -0600

    Small test tweak

--- test/models/first_run_test.rb
+++ test/models/first_run_test.rb
@@ -5,34 +5,24 @@ require "test_helper"
 class FirstRunTest < ActiveSupport::TestCase
   setup do
     Organization.destroy_all
-    Membership.destroy_all
-    Category.destroy_all
-    User.destroy_all
   end
 
   test "creating makes first user an administrator and sets up a new category and organization" do
-    assert_difference "Category.count" do
-      assert_difference "User.count" do
-        assert_difference "Organization.count" do
-          assert_difference "Membership.count" do
-            first_run = FirstRun.new(
-              username: "owner_example",
-              email_address: "owner@example.com",
-              password: "secret123456",
-              organization_name: "Test Organization",
-              organization_subdomain: "testorg",
-              category_name: "Test Category"
-            ).save!
-            organization = first_run.organization
+    assert_difference [ "Category.count", "User.count", "Organization.count", "Membership.count" ] do
+      first_run = FirstRun.new(
+        username: "owner_example",
+        email_address: "owner@example.com",
+        password: "secret123456",
+        organization_name: "Test Organization",
+        organization_subdomain: "testorg",
+        category_name: "Test Category"
+      ).save!
 
-            assert_predicate organization.memberships.first, :administrator?
-            assert_equal "Test Organization", organization.name
-            assert_equal "Test Category", organization.categories.first.name
-            assert_equal "owner@example.com", first_run.user.email_address
-            assert_equal first_run.user.email_address, organization.owner.email_address
-          end
-        end
-      end
+      assert_predicate first_run.organization.memberships.first, :administrator?
+      assert_equal "Test Organization", first_run.organization.name
+      assert_equal "Test Category", first_run.organization.categories.first.name
+      assert_equal "owner@example.com", first_run.user.email_address
+      assert_equal first_run.user.email_address, first_run.organization.owner.email_address
     end
   end
 
