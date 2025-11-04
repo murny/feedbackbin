50a9635f419fa989fb6385ca9ce9ae3dddaa5016
murny <1930474+murny@users.noreply.github.com>
2025-11-04 14:00:58 -0700

    Update text/copy on invite flow

--- app/mailers/invitations_mailer.rb
+++ app/mailers/invitations_mailer.rb
@@ -4,10 +4,11 @@ class InvitationsMailer < ApplicationMailer
   def invite
     @invitation = params[:invitation]
     @invited_by = @invitation.invited_by
+    @organization = Current.organization
 
     mail(
       to: @invitation.email,
-      subject: t(".subject")
+      subject: t(".subject", organization_name: @organization.name)
     )
   end
 end
--- test/mailers/invitations_mailer_test.rb
+++ test/mailers/invitations_mailer_test.rb
@@ -5,11 +5,13 @@ require "test_helper"
 class InvitationsMailerTest < ActionMailer::TestCase
   test "invite" do
     invitation = invitations(:one)
+    organization = Current.organization
     mail = InvitationsMailer.with(invitation: invitation).invite
 
-    assert_equal I18n.t("invitations_mailer.invite.subject"), mail.subject
+    assert_equal I18n.t("invitations_mailer.invite.subject", organization_name: organization.name), mail.subject
     assert_equal [ invitation.email ], mail.to
     assert_match invitation.name, mail.body.encoded
     assert_match invitation.invited_by.name, mail.body.encoded
+    assert_match organization.name, mail.body.encoded
   end
 end
