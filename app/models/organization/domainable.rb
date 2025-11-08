c9d4c387a1c9e8675a231b6c351da225850ee6e8
murny <1930474+murny@users.noreply.github.com>
2025-11-07 17:29:51 -0700

    Fix missing category color input on first run form. Make subdomain in… (#692)
    * Fix missing category color input on first run form. Make subdomain input optional in single tenant mode
    
    * Fix markdown header

--- app/models/organization/domainable.rb
+++ app/models/organization/domainable.rb
@@ -8,13 +8,15 @@ class Organization
       self::SUBDOMAIN_REGEXP = /\A[a-zA-Z0-9]+[a-zA-Z0-9\-_]*[a-zA-Z0-9]+\Z/
       self::RESERVED_SUBDOMAINS = %w[app help support docs blog status www]
 
-      validates :subdomain, presence: true,
+      validates :subdomain, presence: true, if: -> { Rails.application.config.multi_tenant }
+      validates :subdomain,
         exclusion: { in: self::RESERVED_SUBDOMAINS, message: :reserved },
         format: { with: self::SUBDOMAIN_REGEXP, message: :format },
         length: { minimum: 3, maximum: 50 },
-        uniqueness: true
+        uniqueness: true,
+        allow_blank: true
 
-      normalizes :subdomain, with: ->(subdomain) { subdomain.downcase.squish }
+      normalizes :subdomain, with: ->(subdomain) { subdomain&.downcase&.squish }
     end
   end
 end
