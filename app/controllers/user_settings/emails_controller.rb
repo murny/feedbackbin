56a272777df9d41d4c1840de0a66781695b72be7
murny <1930474+murny@users.noreply.github.com>
2025-12-19 14:37:48 -0700

    Refactor email update logic in EmailsController
    - Removed password challenge authentication from the email update process to streamline the update flow.
    - Updated identity_params to include password challenge for better parameter handling.
    - Enhanced error handling by simplifying the response when email address has not changed.

--- app/controllers/user_settings/emails_controller.rb
+++ app/controllers/user_settings/emails_controller.rb
@@ -7,7 +7,7 @@ module UserSettings
     before_action :set_identity
 
     def update
-      if @identity.authenticate(params.dig(:identity, :password_challenge)) && @identity.update(identity_params)
+      if @identity.update(identity_params)
         if @identity.email_address_previously_changed?
           IdentityMailer.email_verification(@identity).deliver_later
           redirect_to user_settings_account_path, notice: t(".email_changed")
@@ -15,7 +15,6 @@ module UserSettings
           redirect_to user_settings_account_path, notice: t(".email_has_not_changed")
         end
       else
-        @identity.errors.add(:password_challenge, :invalid) unless @identity.authenticate(params.dig(:identity, :password_challenge))
         @user = Current.user
         @identity_connected_accounts = Current.identity.identity_connected_accounts.order(provider_name: :asc, created_at: :desc)
         render "user_settings/accounts/show", status: :unprocessable_entity
@@ -25,7 +24,7 @@ module UserSettings
     private
 
       def identity_params
-        params.expect(identity: [ :email_address ])
+        params.expect(identity: [ :email_address, :password_challenge ])
       end
 
       def set_identity
