17072664568b0d6e76c7b951a875420614e9788e
murny <1930474+murny@users.noreply.github.com>
2026-02-20 12:02:03 -0700

    Update dependencies and add new parallel grouping to CI script

--- config/ci.rb
+++ config/ci.rb
@@ -5,18 +5,22 @@
 CI.run do
   step "Setup", "bin/setup"
 
-  step "Style: Ruby", "bin/rubocop"
-  step "Style: ERB", "bin/erb_lint --lint-all"
-  step "Style: I18n", "bin/i18n-tasks health en"
+  group "Checks", parallel: 2 do
+    step "Style: Ruby", "bin/rubocop"
+    step "Style: ERB", "bin/erb_lint --lint-all"
+    step "Style: I18n", "bin/i18n-tasks health en"
 
-  step "Security: Gem audit", "bin/bundler-audit"
-  step "Security: Importmap vulnerability audit", "bin/importmap audit"
-  step "Security: Brakeman code analysis", "bin/brakeman --quiet --no-pager --exit-on-warn --exit-on-error"
-  step "Security: Gitleaks audit", "bin/gitleaks-audit"
+    step "Security: Gem audit", "bin/bundler-audit"
+    step "Security: Importmap vulnerability audit", "bin/importmap audit"
+    step "Security: Brakeman code analysis", "bin/brakeman --quiet --no-pager --exit-on-warn --exit-on-error"
+    step "Security: Gitleaks audit", "bin/gitleaks-audit"
+  end
 
-  step "Tests: Rails", "bin/rails test"
-  step "Tests: System", "bin/rails test:system"
-  step "Tests: Seeds", "env RAILS_ENV=development bin/rails db:seed:replant"
+  group "Tests", parallel: 2 do
+    step "Tests: Rails", "bin/rails test"
+    step "Tests: System", "bin/rails test:system"
+    step "Tests: Seeds", "env RAILS_ENV=development bin/rails db:seed:replant"
+  end
 
   # Optional: set a green GitHub commit status to unblock PR merge.
   # Requires the `gh` CLI and `gh extension install basecamp/gh-signoff`.
