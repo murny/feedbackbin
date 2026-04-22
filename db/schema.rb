655c2c7e475e70c337547f655e49c6cb99ddab35
Shane Murnaghan <1930474+murny@users.noreply.github.com>
2026-04-22 11:00:31 -0600

    Fix schema issue

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.2].define(version: 2026_03_10_000001) do
+ActiveRecord::Schema[8.2].define(version: 2026_03_29_000001) do
   create_table "account_external_id_sequences", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
