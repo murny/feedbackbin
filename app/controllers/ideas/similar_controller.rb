c7faea261978e05fb38c14cefef920aacd8acc98
Shane Murnaghan <1930474+murny@users.noreply.github.com>
2026-06-02 09:06:07 -0600

    feat(03-12): forward idea_id to Idea.similar_to via exclude: (CR-06)
    - Uses .presence so blank/missing param becomes nil
    - Idea.similar_to handles .to_i coercion defensively

--- app/controllers/ideas/similar_controller.rb
+++ app/controllers/ideas/similar_controller.rb
@@ -2,7 +2,12 @@
 
 class Ideas::SimilarController < ApplicationController
   def index
-    @ideas = Idea.similar_to(params[:title], account: Current.account, limit: 3)
+    @ideas = Idea.similar_to(
+      params[:title],
+      account: Current.account,
+      limit: 3,
+      exclude: params[:idea_id].presence
+    )
 
     render partial: "ideas/similar_ideas", locals: { ideas: @ideas, query: params[:title] }
   end
