4a1686f9f63336a9b03fe243adba70f4be60aca7
Shane Murnaghan <1930474+murny@users.noreply.github.com>
2026-06-13 18:15:07 -0600

    test(05-05): add wave 0 mobile touch-target system test
    - Resizes Capybara viewport to 375x812 (iPhone SE), precedent: roadmap_mobile_test.rb
    - Asserts getBoundingClientRect().height >= 44 for 5 fixed surfaces
    - One distinct test per surface group (no loop tests)
    - Wave 0 contract: expected to fail until task 2 lifts the surfaces

---
+++ test/system/ux/mobile_touch_targets_test.rb
@@ -0,0 +1,90 @@
+# frozen_string_literal: true
+
+require "application_system_test_case"
+
+module Ux
+  class MobileTouchTargetsTest < ApplicationSystemTestCase
+    MOBILE_VIEWPORT = [ 375, 812 ].freeze
+    DESKTOP_VIEWPORT = [ 1280, 800 ].freeze
+    MIN_TAP_TARGET_PX = 44
+
+    setup do
+      @account = accounts(:feedbackbin)
+      @user = users(:shane)
+      @idea = ideas(:one)
+
+      page.driver.browser.manage.window.resize_to(*MOBILE_VIEWPORT)
+      sign_in_as(@user)
+    end
+
+    teardown do
+      page.driver.browser.manage.window.resize_to(*DESKTOP_VIEWPORT)
+    end
+
+    test "header mobile toggle button meets 44px on mobile viewport" do
+      visit ideas_url(script_name: @account.slug)
+
+      height = bounding_height(".header__mobile-toggle-btn")
+
+      assert_operator height, :>=, MIN_TAP_TARGET_PX,
+        "expected .header__mobile-toggle-btn to be at least #{MIN_TAP_TARGET_PX}px tall on mobile, got #{height}px"
+    end
+
+    test "header search trigger meets 44px on mobile viewport" do
+      visit ideas_url(script_name: @account.slug)
+
+      height = bounding_height(".header__search-trigger")
+
+      assert_operator height, :>=, MIN_TAP_TARGET_PX,
+        "expected .header__search-trigger to be at least #{MIN_TAP_TARGET_PX}px tall on mobile, got #{height}px"
+    end
+
+    test "vote trigger meets 44px on mobile viewport" do
+      visit idea_url(@idea, script_name: @account.slug)
+
+      height = bounding_height(".vote__trigger")
+
+      assert_operator height, :>=, MIN_TAP_TARGET_PX,
+        "expected .vote__trigger to be at least #{MIN_TAP_TARGET_PX}px tall on mobile, got #{height}px"
+    end
+
+    test "dialog close button meets 44px on mobile viewport" do
+      visit ideas_url(script_name: @account.slug)
+
+      find("body").send_keys("?")
+
+      assert_selector "dialog#keyboard-shortcuts-dialog[open]"
+
+      height = bounding_height(".dialog__close")
+      width = bounding_width(".dialog__close")
+
+      assert_operator height, :>=, MIN_TAP_TARGET_PX,
+        "expected .dialog__close to be at least #{MIN_TAP_TARGET_PX}px tall on mobile, got #{height}px"
+      assert_operator width, :>=, MIN_TAP_TARGET_PX,
+        "expected .dialog__close to be at least #{MIN_TAP_TARGET_PX}px wide on mobile, got #{width}px"
+    end
+
+    test "primary action button meets 44px on mobile viewport" do
+      visit new_idea_url(script_name: @account.slug)
+
+      height = bounding_height("form .btn[type='submit']")
+
+      assert_operator height, :>=, MIN_TAP_TARGET_PX,
+        "expected the submit .btn to be at least #{MIN_TAP_TARGET_PX}px tall on mobile, got #{height}px"
+    end
+
+    private
+
+    def bounding_height(selector)
+      page.evaluate_script(
+        "document.querySelector(#{selector.to_json}).getBoundingClientRect().height"
+      )
+    end
+
+    def bounding_width(selector)
+      page.evaluate_script(
+        "document.querySelector(#{selector.to_json}).getBoundingClientRect().width"
+      )
+    end
+  end
+end
